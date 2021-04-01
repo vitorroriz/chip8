698d054bf0138e2be9fb7d56f7747f97b67d5c47
Vitor Roriz <v.r.roriz@gmail.com>
2021-04-01 15:48:43 +0200

    Add sprites for some fonts in tools

--- src/tools/spriteGenerator.cpp
+++ src/tools/spriteGenerator.cpp
@@ -1,25 +1,35 @@
 #include "spriteGenerator.h"
 #include "spriteGenerator.h"
 
-void Sprite::copyToMemory(uint8_t *location)
+void SpriteGen::Sprite::copyToMemory(uint8_t *location)
 {
 	memcpy(location, lines.data(), _size);
 }
 
-void Sprite::addLine(std::string line)
+void SpriteGen::Sprite::addLine(std::string newLine)
 {
-	uint8_t newLine{ 0 };
+	uint8_t res{ 0 };
 	
-	for (int i = 0; i < line.size(); ++i) {
-		if (line[i] != ' ' || line[i] != '.') {
-			newLine |= (1 << i);
+	for (int i = 0; i < newLine.size(); ++i) {
+		if (newLine[i] != ' ' &&  newLine[i] != '.' && newLine[i] != '0') {
+			res |= (1 << (7 - i));
 		}
 	}
 
+	lines.push_back(res);
 	++_size;
 }
 
-void Sprite::addLines(std::string line)
+void SpriteGen::Sprite::addLines(std::string newLines)
 {
+	int limit = newLines.size() % 8 ? newLines.size() + 1 : newLines.size();
+	for (int i = 0; i < limit / 8; ++i) {
+		auto line = newLines.substr(i * 8, 8);
+		if (line.empty()) {
+			break;
+		}
+		addLine(line);
+	}
 }
+
 
--- src/tools/spriteGenerator.h
+++ src/tools/spriteGenerator.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <vector>
 #include <string>
-
+namespace SpriteGen {
 class Sprite
 {
 public:
 	Sprite() : _size{ 0 }
 	{
+	}
+
+	Sprite(std::string lines) : _size{ 0 }
+	{
+		addLines(lines);
 	}
 
 	~Sprite()
@@ -17,14 +22,122 @@ public:
 
 	void copyToMemory(uint8_t* location);
 	//Each line is 8 chars, more than that will generate a new line
-	void addLine(std::string line); 
-	void addLines(std::string line);
+	void addLine(std::string newline);
+	void addLines(std::string newLines);
+	const uint8_t* data() { return lines.data(); }
 
 private:
 	size_t _size; //number (bytes) of sprite. Each byte is a line.
 	std::vector<uint8_t> lines;
 };
 
+	const std::string FONT_A = {
+	"11111   "
+	"1   1   "
+	"11111   "
+	"1   1   "
+	"1   1   "
+	};
+	const std::string FONT_a = {
+	"        "
+	"1111    "
+	"1  1    "
+	"1  1    "
+	"11111   "
+	};
+	const std::string FONT_B = {
+	"111     "
+	"1  1    "
+	"111     "
+	"1  1    "
+	"111     "
+	};
+
+	const std::string FONT_b = {
+	"1       "
+	"1       "
+	"111     "
+	"1  1    "
+	"111     "
+	};
+	const std::string FONT_C = {
+	" 111    "
+	"1   1   "
+	"1       "
+	"1   1   "
+	" 111    "
+	};
+	const std::string FONT_c = {
+	"        "
+	"        "
+	"1111    "
+	"1       "
+	"1111    "
+	};
+	const std::string FONT_D = {
+	"1111    "
+	"1   1   "
+	"1   1   "
+	"1   1   "
+	"1111    "
+	};
+	const std::string FONT_d = {
+	"   1    "
+	"   1    "
+	"1111    "
+	"1  1    "
+	"1111    "
+	};
+	const std::string FONT_e = {
+	"11111   "
+	"1       "
+	"11111   "
+	"1       "
+	"11111   "
+	};
+	const std::string FONT_E = {
+	" 111    "
+	"1   1   "
+	"11111   "
+	"1       "
+	"1111    "
+	};
+	const std::string FONT_V = {
+	"1   1   "
+	"1   1   "
+	" 1 1    "
+	" 1 1    "
+	"  1     "
+	};
+	const std::string FONT_I = {
+	"1       "
+	"        "
+	"1       "
+	"1       "
+	"1       "
+	};
+	const std::string FONT_T = {
+	"11111   "
+	"  1     "
+	"  1     "
+	"  1     "
+	"  1     "
+	};
+	const std::string FONT_O = {
+	" 1111   "
+	"1    1  "
+	"1    1  "
+	"1    1  "
+	" 1111   "
+	};
+	const std::string FONT_R = {
+	"1111    "
+	"1  1    "
+	"1 1     "
+	"1  1    "
+	"1   1   "
+	};
+}
 
 
 
